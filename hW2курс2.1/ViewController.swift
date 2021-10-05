35b721db16f2ed4a2b29c82bd28c0c51285a025f
leha9321 <leha93.21@icloud.com>
2021-10-05 21:49:06 +0300

    Initial Commit

---
+++ hW2курс2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hW2курс2.1
+//
+//  Created by Алексей Трофимов on 05.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
